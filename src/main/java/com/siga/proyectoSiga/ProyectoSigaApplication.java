bcb221c10ebce2b9c3aab55772224d404bc23ccf
jorgegalvez <jo.galvezr@duocuc.cl>
2025-06-18 11:31:41 -0400

    primer commit

---
+++ src/main/java/com/siga/proyectoSiga/ProyectoSigaApplication.java
@@ -0,0 +1,13 @@
+package com.siga.proyectoSiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSigaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siga/proyectoSiga/ProyectoSigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siga.proyectoSiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
